6fed33d9e7025ebc70acc6a0fba125c230515814
Niko Lazaris <nikolazaris@gmail.com>
2021-05-10 23:30:25 -0400

    Update redirect message

--- lib/middleman-prismic/preview.rb
+++ lib/middleman-prismic/preview.rb
@@ -14,7 +14,7 @@ module Middleman
           token = req.params["token"]
 
           if token.nil?
-            return [302, {"Location" => "/?error=preview_failure"}, ["Invalid token"]]
+            return [302, {"Location" => "/"}, ["Token required"]]
           end
 
           begin
